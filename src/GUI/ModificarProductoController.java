4ee39fede9ae7bc861b54fdab2660c2138ba040f
EderIvan805 <85649939+EderIvan805@users.noreply.github.com>
2022-05-23 16:44:28 -0500

    Nuevas interfaces del producto (Agregar y Modificar)
    Se agregaron los FXML para los CU:
    - Agregar producto
    - Modificar producto

---
+++ src/GUI/ModificarProductoController.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package GUI;
+
+import java.awt.Button;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javax.swing.text.html.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author eder_
+ */
+public class ModificarProductoController implements Initializable {
+    @FXML
+    private Label label;
+    @FXML
+    private Button button;
+    @FXML
+    private TextField txfNombreProducto;
+    @FXML
+    private TextField txfPrecio;
+    @FXML
+    private TextField txfTiempoAproximado;
+    @FXML
+    private TextField txfDescripcion;
+    @FXML
+    private ImageView imgProducto;
+    
+   
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void clicCancelar(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicGuardarCambios(ActionEvent event) {
+    }
+
+    @FXML
+    private void clicElegirImagen(ActionEvent event) {
+    }
+    
+    private boolean existenCamposInvalidos(){
+        boolean existe = false;
+        MensajeAlerta mensajeAlerta = new MensajeAlerta();
+        Validacion validacion = new Validacion();
+        
+        if(txfNombreProducto.getText().isEmpty() || txfPrecio.getText().isEmpty() || txfTiempoAproximado.getText().isEmpty() || txfDescripcion.getText().isEmpty()){
+            existe = true;
+            mensajeAlerta.mostrarAlertaInformacionInvalida("Existen campos vacíos");
+        } 
+        
+        if(validacion.existeCampoInvalido(txfNombreProducto.getText()) || validacion.existeCampoInvalido(txfPrecio.getText()) 
+                || validacion.existeCampoInvalido(txfTiempoAproximado.getText()) || validacion.existeCampoInvalido(txfDescripcion.getText())){
+            existe = true;
+            mensajeAlerta.mostrarAlertaInformacionInvalida("Existen caracteres inválidos");
+        }
+        
+        return existe;
+    }
+    
+}
